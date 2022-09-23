f4e970c65e02f6b3f5fa84e06f5cac04d498c844
Nikita Lyakhovoy <lyakhovoynikita@gmail.com>
2022-09-24 00:16:33 +0100

    Set bg to black

--- src/components/container/container.styles.js
+++ src/components/container/container.styles.js
@@ -1,10 +1,10 @@
 export const root = {
   width: "100%",
   minHeight: "100vh",
-  //backgroundColor: "tertiary.main",
+  backgroundColor: "tertiary.main",
   //backgroundImage: `url(${require("../../assets/night_waterfall.webp")})`,
   //backgroundSize: 'cover',
-  // backgrounRepeat: 'noRepeat',
+  //backgrounRepeat: 'noRepeat',
   px: { md: "56px" },
   pt: { md: "100px", xs: "70px" },
   maxWidth: "1900px",
