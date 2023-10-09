5ab1400ca41f7feffed0a5cdcfc297abab858ca7
Nikita Lyakhovoy <lyakhovoynikita@gmail.com>
2023-10-10 00:25:14 +0100

    Update mint frame

--- src/components/heroSectionText/index.js
+++ src/components/heroSectionText/index.js
@@ -91,13 +91,12 @@ const HeroSectionText = ({ accounts, setAccounts }) => {
           </motion.div> */}
 
           <iframe
-              title="mintFrame"
-              src="https://gateway.ipfscdn.io/ipfs/QmQM4Njtt2o4cQ98Mi2kBD6rBsfH6LfeaRYBYLpR41nVFs/signature-drop.html?contract=0xdEB04F0c3a13954c6CDB7078E710d04c465eAb01&chainId=1&theme=dark&primaryColor=orange"
+              src="https://embed.ipfscdn.io/ipfs/bafybeicd3qfzelz4su7ng6n523virdsgobrc5pcbarhwqv3dj3drh645pi/?contract=0xdEB04F0c3a13954c6CDB7078E710d04c465eAb01&chain=%7B%22name%22%3A%22Ethereum+Mainnet%22%2C%22chain%22%3A%22ETH%22%2C%22rpc%22%3A%5B%22https%3A%2F%2Fethereum.rpc.thirdweb.com%2F%24%7BTHIRDWEB_API_KEY%7D%22%5D%2C%22nativeCurrency%22%3A%7B%22name%22%3A%22Ether%22%2C%22symbol%22%3A%22ETH%22%2C%22decimals%22%3A18%7D%2C%22shortName%22%3A%22eth%22%2C%22chainId%22%3A1%2C%22testnet%22%3Afalse%2C%22slug%22%3A%22ethereum%22%2C%22icon%22%3A%7B%22url%22%3A%22ipfs%3A%2F%2FQmcxZHpyJa8T4i63xqjPYrZ6tKrt55tZJpbXcjSDKuKaf9%2Fethereum%2F512.png%22%2C%22width%22%3A512%2C%22height%22%3A512%2C%22format%22%3A%22png%22%7D%7D&clientId=9b1799ad085d88269e67f2404b6e944b&theme=dark&primaryColor=orange"
               width="600px"
               height="600px"
-              style={{ maxWidth:"100%"}}
-              frameBorder="0"
-              ></iframe>
+              style="max-width:100%;"
+              frameborder="0"
+          ></iframe>
 
           {/* {isConnected ? (
                 <Box>
